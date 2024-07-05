b86425532440a8677354bd8de4c15f0c5aa950d5
Marcin Hanas <mahanas@outlook.com>
2024-07-06 00:43:26 +0200

    refactor: Remove unused import
    - Removed unused import of `ExperimentalMaterial3Api` from MainActivity.kt.

--- app/src/main/java/com/hanas/addy/MainActivity.kt
+++ app/src/main/java/com/hanas/addy/MainActivity.kt
@@ -1,11 +1,8 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.hanas.addy
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.hanas.addy.home.Home
