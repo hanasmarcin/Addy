43bf91a02bba99c713cce950f893a848d14395ac
Marcin Hanas <mahanas@outlook.com>
2024-07-18 10:49:19 +0200

    refactor: Introduce AppButton component
    This commit introduces a new AppButton component to provide a consistent and reusable button implementation throughout the app.

--- app/src/main/java/com/hanas/addy/home/HomeScreen.kt
+++ app/src/main/java/com/hanas/addy/home/HomeScreen.kt
@@ -4,12 +4,8 @@ import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.IconButton
@@ -23,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +27,7 @@ import androidx.navigation.compose.composable
 import coil.compose.AsyncImage
 import com.hanas.addy.R
 import com.hanas.addy.ui.AppTheme
+import com.hanas.addy.ui.components.AppButton
 import com.hanas.addy.ui.components.AppScaffold
 import kotlinx.serialization.Serializable
 import org.koin.androidx.compose.navigation.koinNavViewModel
@@ -96,37 +92,18 @@ fun HomeScreen(
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            HomeMenuButton(
-                title = "Play",
+            AppButton(
+                onClick = {},
                 color = MaterialTheme.colorScheme.primaryContainer
-            ) {}
-            HomeMenuButton(
-                title = "Your card stacks",
+            ) {
+                Text("Play")
+            }
+            AppButton(
+                onClick = { navHandler.navigate(CardStackList, true) },
                 color = MaterialTheme.colorScheme.tertiaryContainer
             ) {
-                navHandler.navigate(CardStackList, true)
+                Text("Your card stacks")
             }
-        }
-    }
-}
-
-@Composable
-fun HomeMenuButton(
-    title: String,
-    color: Color = MaterialTheme.colorScheme.surfaceContainer,
-    onClick: () -> Unit
-) {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        colors = cardColors(color),
-        onClick = onClick
-    ) {
-        Row {
-            Text(
-                text = title,
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.titleLarge
-            )
         }
     }
 }
